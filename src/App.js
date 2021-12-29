0aa61389d93dafd53d95ed7ddb647b162aba5604
MarquisPierre <marquispierre27@gmail.com>
2021-12-29 09:55:15 -0500

    final commit, I will become a React pro!

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/marquispierre"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My GitHub 
         </a>
       </header>
     </div>
