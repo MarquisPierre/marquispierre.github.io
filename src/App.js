7cd879053b5b1bfc34d8de38c7578b429660fb8e
MarquisPierre <marquispierre27@gmail.com>
2022-01-03 21:21:42 -0500

    updated the HashRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route ,Switch } from "react-router-dom";
 import { HashRouter as Router } from 'react-router-dom'
 import Home from './components/Home';
 import About from './components/About';
